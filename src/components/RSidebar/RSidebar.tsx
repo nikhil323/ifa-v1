f7b0d94c1cddaeeef9df9688bcee2cad2d3cef03
Nikhil Mahara <nikhil323mahara@gmail.com>
2023-11-27 10:39:38 +0545

    updated rsidebar

--- src/components/RSidebar/RSidebar.tsx
+++ src/components/RSidebar/RSidebar.tsx
@@ -4,14 +4,25 @@ import React from "react";
 import rsidebarStyles from "./rsidebarStyles.module.css";
 
 const RSidebar = () => {
-  return <div className={rsidebarStyles.container}>
-    <div>
-      <h4>Points to ponder:</h4>
-      <div>Carefully go through vacancy post and organization</div>
-      <div>Research about organization</div>
-      <div>Before applying see if criterias and requirements are met</div>
+  return (
+    <div className={rsidebarStyles.container}>
+      <div className={rsidebarStyles.inner}>
+        <h4 className={rsidebarStyles.title}>Points to ponder:</h4>
+        <ul className={rsidebarStyles.points}>
+          <li>Carefully go through vacancy post and organization</li>
+          <li>Research about organization</li>
+          <li>Before applying see if criterias and requirements are met</li>
+          <li>See if the organization is verified or not</li>
+          <li>Report any harmful/ill intent activities</li>
+          <li>Be cautious about spam, fake organizations and users </li>
+          <li>
+            Feel free to provide feedbacks and suggestions so we can improve
+            user activities
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default RSidebar;
